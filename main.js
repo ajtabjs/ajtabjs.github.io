b76cf7fec52fc753b887f4596e8320f0c4453325
ajtabjs <oro10jua@gmail.com>
2023-06-30 20:47:29 -0700

    some other stuff

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-window.addEventListener("load", function() {
+/* window.addEventListener("load", function() {
   var element = document.getElementById("title");
   element.classList.add("intro-fade-in");
+}); */
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("title").classList.add("intro-fade-in");
 });
 
 //test
