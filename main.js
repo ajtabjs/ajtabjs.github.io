f5536fb65a2c782b8cf5bd00adfa7c7c4e6f6dd0
unknown <oro10jua@gmail.com>
2024-09-08 15:42:52 -0700

    loading screen, thanks top

--- main.js
+++ main.js
@@ -9,5 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
   worldtitle.classList.add("intro-fade-in");
   
 }); */
+/*
+var myVar;
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
 
+function showPage() {
+  document.getElementById("container").style.display = "none";
+  document.getElementsByTagName("body").style.display = "block";
+} */
 //test
